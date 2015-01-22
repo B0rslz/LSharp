d8071127ecf5d09a87901c4c468658c8a617e104
DarkAzazel <xmarcolinho90x@hotmail.it>
2015-01-22 17:14:55 +0100

    Update Program.cs

--- Maokai/Program.cs
+++ Maokai/Program.cs
@@ -214,7 +214,7 @@ namespace Maokai
         {
 
 
-            int inimigos = Utility.CountEnemysInRange(Player, 500);
+            int inimigos = Utility.CountEnemysInRange(Player, 650);
 
             var RMana = Config.Item("ManaR").GetValue<Slider>().Value;
             var MPercentR = Player.Mana * 100 / Player.MaxMana;
